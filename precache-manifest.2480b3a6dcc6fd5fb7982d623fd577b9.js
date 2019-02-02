fab305c34a29e809eb3ed436facda852d33c44db
Maggie Rose <maggie.rose.g@gmail.com>
2019-02-01 18:08:00 -0600

    Updates

---
+++ precache-manifest.2480b3a6dcc6fd5fb7982d623fd577b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "efe408cdcb3570bd8728",
+    "url": "/contactmanager/static/js/main.efe408cd.chunk.js"
+  },
+  {
+    "revision": "453495bcda42829f74ba",
+    "url": "/contactmanager/static/js/1.453495bc.chunk.js"
+  },
+  {
+    "revision": "efe408cdcb3570bd8728",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "453495bcda42829f74ba",
+    "url": "/contactmanager/static/css/1.49670344.chunk.css"
+  },
+  {
+    "revision": "b050bf5926ff0409dfd6a1b87188d4dd",
+    "url": "/contactmanager/index.html"
+  }
+];
